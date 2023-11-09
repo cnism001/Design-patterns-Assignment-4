581fe2a56d9b137a7e005a3cec5e0284a14a9c33
Nikita Smirnov <cnism001@edu.xamk.fi>
2023-11-09 15:55:29 +0200

    Added condiments subclass

---
+++ DecoratorAssignment/Condiments.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorAssignment
+{
+    //Subclass to hold the condiments for Beverages-class objects
+    public abstract class Condiment : Beverage
+    {
+        //gets description from condiments:sublacc members and doesnt interfere with Beverage description
+        public abstract override string Description { get; }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
