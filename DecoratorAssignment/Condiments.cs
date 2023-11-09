0e6db453a9360d5ad0c63081ce7ca3eaeb0c0efa
Nikita Smirnov <cnism001@edu.xamk.fi>
2023-11-09 16:18:57 +0200

    added SteamedMilk class and constructor, renamed some variables for more clarity

--- DecoratorAssignment/Condiments.cs
+++ DecoratorAssignment/Condiments.cs
@@ -12,8 +12,26 @@ namespace DecoratorAssignment
         //gets description from condiments:sublacc members and doesnt interfere with Beverage description
         public abstract override string Description { get; }
     }
+    //Subclass inheriting from Condiment subclass
+    public class SteamedMilk : Condiment
+    {   
+        //Beverage object, can only be set during initialization
+        private readonly Beverage _beverageComponent;
 
+        //simple constructor for SteamedMilk
+        public SteamedMilk(Beverage beverageComponent)
+        {
+            _beverageComponent = beverageComponent;
+        }
+        //Appends  Beverage description with description of the condiment
+        public override string Description => _beverageComponent.Description + ", wih Steamed Milk";
 
+        //Adds the cost of the condiment to the cost of the Beverage
+        public override double Cost()
+        {
+            return _beverageComponent.Cost() + 0.20;
+        }
+    }
 
 
 
