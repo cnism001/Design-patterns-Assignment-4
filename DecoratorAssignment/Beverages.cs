bac600b6b6229b10595a9a7012f22005e0fcc77c
Nikita Smirnov <cnism001@edu.xamk.fi>
2023-11-09 15:36:07 +0200

    Added class for Beverages with description and cost variable and several subclasses inheriting its members

---
+++ DecoratorAssignment/Beverages.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorAssignment
+{
+    public abstract class Beverage
+    {
+        //Description can be retrieved by anyone, can only be set from whithin Beverage class
+        public string Description { get; protected set; } = " ";
+
+        //cost variable to use in sublasses
+        public abstract double Cost();
+    }
+    //HouseBlend subclass with description and price
+    public class HouseBlend : Beverage
+    {
+        public HouseBlend()
+        {
+            Description = "House Blend Coffee";
+        }
+
+        public override double Cost()
+        {
+            return 0.89;
+        }
+    }
+
+    //DarkRoast subclass with description and price
+    public class DarkRoast : Beverage
+    {
+        public DarkRoast()
+        {
+            Description = "Dark Roast Coffee";
+        }
+
+        public override double Cost()
+        {
+            return 0.99;
+        }
+    }
+
+    //Decaf subclass with description and price
+    public class Decaf : Beverage
+    {
+        public Decaf()
+        {
+            Description = "Decaf Coffee";
+        }
+
+        public override double Cost()
+        {
+            return 1.05;
+        }
+    }
+
+    //Espresso subclass with description and price
+    public class Espresso : Beverage
+    {
+        public Espresso()
+        {
+            Description = "Espresso";
+        }
+
+        public override double Cost()
+        {
+            return 1.99;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- DecoratorAssignment/Program.cs
+++ DecoratorAssignment/Program.cs
@@ -11,7 +11,7 @@
     {
         static void Main(string[] args)
         {
-            
+        
         }
     }
 }
